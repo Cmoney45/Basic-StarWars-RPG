df0f4410442fa215f67cd5cc50be125e6f125a7b
Cmoney <cascherer42@gmail.com>
2018-07-25 18:33:51 -0500

    initial pseudo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+var starWarsRPG = {
+    characters = ["Luke","Obi-wan","Darth Maul","Darth Sidious"],
+    // on load, create imgs for each and append to the top div
+        //add a $click to move to Your character div and move the other cards to the enemies available to attack div
+        //when in enemies div, have $click to move to defender div and if there is a defender there, do nothing on click
+    
+    //$button click for attack to grab the attack values for hero and defender and subtract for health appropriately. 
+        //If heroHP <= 0 lose, else if enemy hp is <=0 remove from game / hide, else reduce hp by attack values
+        //If lose or all enemies are gone, show reset button
+
+    //reset button function to be called on reset click. Set all game values back to original.
+
+}
