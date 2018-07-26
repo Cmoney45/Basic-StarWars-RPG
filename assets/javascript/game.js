f0a9f5695bffafee11bbb4973b3937363cb42b59
Cmoney <cascherer42@gmail.com>
2018-07-26 17:06:39 -0500

    added images, loaded character models to first position. Need to have hp showing then can move on to click events

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,19 @@
 var starWarsRPG = {
-    characters = ["Luke","Obi-wan","Darth Maul","Darth Sidious"],
+//     CharCreator: function(name,health,attackDmg,model) {
+//     this.name = name;
+//     this.health = health;
+//     this.attackDmg = attackDmg;
+//     this.model = model;
+// },
+    characters: {
+    luke: {name: "luke", charName:"Luke" , health: 100, damage: 5, model: "assets/images/characters/LukeSkywalker.png"},
+    obiwan: {name: "obiwan", charName:"Obi-Wan", health: 120 ,damage: 8, model: "assets/images/characters/obiwankenobi.webp"},
+    maul: {name: "maul", charName:"Darth Maul", health: 180, damage: 25, model: "assets/images/characters/DarthMaul.jpeg"},
+    sidious: {name: "sidious", charName:"Darth Sidious", health: 150, damage: 20, model: "assets/images/characters/darthSidious.jpg"}
+    }
+}  
+
+    
     // on load, create imgs for each and append to the top div
         //add a $click to move to Your character div and move the other cards to the enemies available to attack div
         //when in enemies div, have $click to move to defender div and if there is a defender there, do nothing on click
@@ -9,5 +23,26 @@ var starWarsRPG = {
         //If lose or all enemies are gone, show reset button
 
     //reset button function to be called on reset click. Set all game values back to original.
+for (var key in starWarsRPG.characters) {
+
+    var obj = starWarsRPG.characters[key];
+
+    var characterBox = $("<div>");
+
+    characterBox.addClass("characterBox");
+    characterBox.attr("data-damage", obj["damage"]);
+    characterBox.attr("data-health", obj["health"]);
+    characterBox.attr("data-name", obj["charName"]);
+    characterBox.text(obj["charName"])
+    characterBox.attr("id", obj["name"]);
+
+    var characterBoxImg = $("<img>");
+
+    characterBoxImg.addClass("characterBoxImg");
+    characterBoxImg.attr("src", obj["model"]);
+
+    
+    $("#begin-location").append(characterBox);
+    $(`#${obj["name"]}`).append(characterBoxImg);
 
 }
