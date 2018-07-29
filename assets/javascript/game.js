646fa3b815729809d231c1cab3a8619bdffe4efc
Cmoney <cascherer42@gmail.com>
2018-07-28 19:27:07 -0500

    Completed functions of game. Still needs a little style.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,64 +6,174 @@ var starWarsRPG = {
 //     this.model = model;
 // },
     characters: {
-    luke: {name: "luke", charName:"Luke" , health: 100, damage: 5, model: "assets/images/characters/LukeSkywalker.png"},
-    obiwan: {name: "obiwan", charName:"Obi-Wan", health: 120 ,damage: 8, model: "assets/images/characters/obiwankenobi.webp"},
-    maul: {name: "maul", charName:"Darth Maul", health: 180, damage: 25, model: "assets/images/characters/DarthMaul.jpeg"},
-    sidious: {name: "sidious", charName:"Darth Sidious", health: 150, damage: 20, model: "assets/images/characters/darthSidious.jpg"}
+    luke: {
+        name: "luke", 
+        charName:"Luke Skywalker" , 
+        health: 100, 
+        damage: 14, 
+        model: "assets/images/characters/LukeSkywalker.png"},
+    obiwan: {
+        name: "obiwan", 
+        charName:"Obi-Wan Kenobi", 
+        health: 120 ,
+        damage: 10, 
+        model: "assets/images/characters/obiwankenobi.webp"},
+    maul: {
+        name: "maul", 
+        charName:"Darth Maul", 
+        health: 180, 
+        damage: 25, 
+        model: "assets/images/characters/DarthMaul.jpeg"},
+    sidious: {
+        name: "sidious", 
+        charName:"Darth Sidious", 
+        health: 150, 
+        damage: 20, 
+        model: "assets/images/characters/darthSidious.jpg"}
     },
     attacker: {
         attackerCount: 0,
         attackersAllowed: 1,
         id: "",
+        attackerName: "",
+        health: 0,
+        damage: 0,
+        staticDamage: 0,
+        attackCalculation: function(){
+            var attackValue = this.damage;
+            this.damage = this.damage + this.staticDamage;
+            return attackValue;
+        }
+    },
+    defender: {
+        defenderCount:0,
+        defendersAllowed: 1,
+        id: "",
+        defenderName: "",
         health: 0,
         damage: 0,
     },
-    defender: {
-        defendersAllowed: 1,
-        id: "",
-        health: 0,
-        damage: 0,
-    }
 }  
 
-    
-    // on load, create imgs for each and append to the top div
-        //add a $click to move to Your character div and move the other cards to the enemies available to attack div
-        //when in enemies div, have $click to move to defender div and if there is a defender there, do nothing on click
-    
-    //$button click for attack to grab the attack values for hero and defender and subtract for health appropriately. 
-        //If heroHP <= 0 lose, else if enemy hp is <=0 remove from game / hide, else reduce hp by attack values
-        //If lose or all enemies are gone, show reset button
+let characterCount = Object.keys(starWarsRPG.characters).length;
 
-    //reset button function to be called on reset click. Set all game values back to original.
-for (var key in starWarsRPG.characters) {
 
-    var obj = starWarsRPG.characters[key];
+function resetGame() {
+    starWarsRPG.attacker.attackerCount = 0;
+    starWarsRPG.defender.defenderCount = 0;
+    characterCount = Object.keys(starWarsRPG.characters).length - 1;
 
-    var characterBox = $("<div>");
 
-    characterBox.addClass("characterBox");
-    characterBox.attr("data-damage", obj["damage"]);
-    characterBox.attr("data-health", obj["health"]);
-    characterBox.attr("data-name", obj["charName"]);
-    characterBox.text(obj["charName"])
-    characterBox.attr("id", obj["name"]);
+        for (var key in starWarsRPG.characters) {
 
-    var characterBoxImg = $("<img>");
+            var obj = starWarsRPG.characters[key];
 
-    characterBoxImg.addClass("characterBoxImg");
-    characterBoxImg.attr("src", obj["model"]);
+            var characterBox = $("<div>");
 
-    
-    $("#begin-location").append(characterBox);
-    $(`#${obj["name"]}`).append(characterBoxImg);
+            characterBox.addClass("characterBox");
+            characterBox.attr("data-damage", obj["damage"]);
+            characterBox.attr("data-health", obj["health"]);
+            characterBox.attr("data-name", obj["charName"]);
+            characterBox.text(obj["charName"])
+            characterBox.attr("id", obj["name"]);
 
+            var characterBoxImg = $("<img>");
+
+            characterBoxImg.addClass("characterBoxImg");
+            characterBoxImg.attr("src", obj["model"]);
+
+            $("#begin-location").append(characterBox);
+            $(`#${obj["name"]}`).append(characterBoxImg);
+            $(`#${obj["name"]}`).append(`<div id=${obj["name"]}healthPool>${obj["health"]}</div>`)
+
+        }
 }
 
-$(".characterBox").click(function(){
-    if (starWarsRPG.attacker.attackerCount < starWarsRPG.attacker.attackersAllowed){
-        starWarsRPG.attacker.attackerCount++ 
-        $("#selected-character").append(this);
-        $("non-selected-character").append(".characterBox");
+resetGame();
+
+$("body").on("click","#attack", function(){
+    let attacker = starWarsRPG.attacker;
+    let defender = starWarsRPG.defender;
+
+    if( attacker.attackerCount < 1){
+        $("#attack-dialogue").text("Please choose a character to play as.");
+    } else if (defender.defenderCount < 1) {
+        $("#attack-dialogue").text("Please choose a character to attack.");
+    } else {
+        var attackerDmg = attacker.attackCalculation();
+        var defenderDmg = defender.damage;
+        defender.health = defender.health - attackerDmg;
+        $(`#${defender.id}healthPool`).html(defender.health);
+        if (defender.health <= 0){
+            $(".currentEnemy").removeClass("enemy")
+            $(".currentEnemy").remove();
+            defender.defenderCount = 0;
+            characterCount--;
+
+            if( characterCount === 0){
+                $("#attack-dialogue").text("Congratulations! You've defeated all enemies you are the Jedi Master!");
+                $(".resetButton").removeAttr("id");
+            } else {
+            $("#attack-dialogue").text(`Well done! You defeated ${defender.id}! Choose another enemy to fight!`)
+            }
+
+        } else { 
+            attacker.health = attacker.health - defenderDmg;
+            if (attacker.health <= 0){
+                attacker.health = 0;
+                $(`#${attacker.id}healthPool`).html(attacker.health);
+                $("#attack-dialogue").html(`You have been defeated by ${defender.defenderName}. You lose!`);
+                $(".resetButton").removeAttr("id");
+            } else {
+                $(`#${attacker.id}healthPool`).html(attacker.health);
+                $("#attack-dialogue").html(`You attacked ${defender.defenderName} for ${attackerDmg} damage.<br>
+                ${defender.defenderName} attacked you for ${defenderDmg} damage.`)
+            }
+        }
     }
+})
+
+$("body").on("click", ".characterBox", function(){
+    let attacker = starWarsRPG.attacker;
+
+    if (attacker.attackerCount < attacker.attackersAllowed){
+        attacker.attackerCount++ 
+        $("#non-selected-character").append($(".characterBox"));
+        $(".characterBox").addClass("enemy")
+        $("#selected-character").append(this);
+        $(this).removeClass("enemy");
+        $(this).addClass("playerCharacter");
+        attacker.id = $(this).attr("id");
+        attacker.attackerName = $(this).attr("data-name");
+        attacker.health = parseInt($(this).attr("data-health"));
+        attacker.damage = parseInt($(this).attr("data-damage"));
+        attacker.staticDamage = parseInt($(this).attr("data-damage"));
+
+    }
+})
+
+$("body").on("click", ".enemy", function(){
+    let defender = starWarsRPG.defender;
+
+    if (defender.defenderCount < defender.defendersAllowed){
+        defender.defenderCount++;
+        $("#current-enemy").append(this);
+        $(this).addClass("currentEnemy");
+        defender.defenderName = $(this).attr("data-name");
+        defender.id = $(this).attr("id");
+        defender.health = parseInt($(this).attr("data-health"));
+        defender.damage = parseInt($(this).attr("data-damage"));
+        $("#attack-dialogue").html(`You have selected ${defender.id} to be your opponent.<br>
+        Click the attack button to start the fight.`)
+
+    }
+})
+
+$("body").on("click",".resetButton", function(){
+    $("#attack-dialogue").empty();
+    $("#selected-character").empty();
+    $("#non-selected-character").empty();
+    $("#current-enemy").empty();
+    resetGame();
+    $(".resetButton").attr("id","resetKey");
 })
